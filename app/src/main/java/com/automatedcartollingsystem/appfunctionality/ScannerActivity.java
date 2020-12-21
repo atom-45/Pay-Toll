79b397777d94099581da152fa94828c0a48c4a41
Matome <matomemodiba3@gmail.com>
2020-12-21 15:09:13 +0200

    app functionality classes

---
+++ app/src/main/java/com/automatedcartollingsystem/appfunctionality/ScannerActivity.java
@@ -0,0 +1,18 @@
+package com.automatedcartollingsystem.appfunctionality;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.automatedcartollingsystem.R;
+
+public class ScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+
+        //Use both and toast and notifications to alert the user to
+    }
+}
---
+++ app/src/main/java/com/automatedcartollingsystem/appfunctionality/TrafficUpdateActivity.java
@@ -0,0 +1,16 @@
+package com.automatedcartollingsystem.appfunctionality;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.automatedcartollingsystem.R;
+
+public class TrafficUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traffic_update);
+    }
+}
