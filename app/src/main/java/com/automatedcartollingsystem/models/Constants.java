e72dfc2919b8f55925283305320d65fdedda579e
Matome <matomemodiba3@gmail.com>
2021-01-02 19:06:45 +0200

    added url for account_acts_script

--- app/src/main/java/com/automatedcartollingsystem/models/Constants.java
+++ app/src/main/java/com/automatedcartollingsystem/models/Constants.java
@@ -3,6 +3,7 @@ package com.automatedcartollingsystem.models;
 public class Constants {
 
     public static String URL_USER_STRING = "http://192.168.8.105/user_acts_script.php"; //This is not secured.
+    public static String URL_ACCOUNT_STRING = "http://192.168.8.105/account_acts_script.php";
     public static String URL_CAR_STRING = "";
     public static String URL_TRANSACTION_STRING = "";
 }
