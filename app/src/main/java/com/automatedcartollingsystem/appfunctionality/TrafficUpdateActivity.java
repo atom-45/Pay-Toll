1e705b5cd5451c35b9ac767ec5db4cddb7c68472
Matome <matomemodiba3@gmail.com>
2021-07-30 21:25:20 +0200

    enabled the up button

--- app/src/main/java/com/automatedcartollingsystem/appfunctionality/TrafficUpdateActivity.java
+++ app/src/main/java/com/automatedcartollingsystem/appfunctionality/TrafficUpdateActivity.java
@@ -1,5 +1,6 @@
 package com.automatedcartollingsystem.appfunctionality;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,5 +13,10 @@ public class TrafficUpdateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traffic_update);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 }
--- app/src/main/java/com/automatedcartollingsystem/appfunctionality/TransactionHistoryActivity.java
+++ app/src/main/java/com/automatedcartollingsystem/appfunctionality/TransactionHistoryActivity.java
@@ -1,5 +1,6 @@
 package com.automatedcartollingsystem.appfunctionality;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,5 +13,10 @@ public class TransactionHistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_history);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 }
