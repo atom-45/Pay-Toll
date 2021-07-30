9ec4cf07e8b9bb8950e9d2b4c71159eaa869b653
Matome <matomemodiba3@gmail.com>
2021-07-30 21:33:13 +0200

    created a database helper and an activity that enables the user to add/update cars

--- app/src/main/java/com/automatedcartollingsystem/database/ActsDatabaseHelper.java
+++ app/src/main/java/com/automatedcartollingsystem/database/ActsDatabaseHelper.java
@@ -1,2 +1,74 @@
-package com.automatedcartollingsystem.database;public class ActsDatabaseHelper {
+package com.automatedcartollingsystem.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class ActsDatabaseHelper  extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "user_email";
+    private static final int OLD_VERSION = 1;
+    private static final int NEW_VERSION = 2;
+
+    public ActsDatabaseHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, OLD_VERSION);
+    }
+
+    /**
+     * Called when the database is created for the first time. This is where the
+     * creation of tables and the initial population of the tables should happen.
+     *
+     * @param db The database.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        updateDatabase(db,OLD_VERSION,NEW_VERSION);
+    }
+
+    /**
+     * Called when the database needs to be upgraded. The implementation
+     * should use this method to drop tables, add tables, or do anything else it
+     * needs to upgrade to the new schema version.
+     *
+     * <p>
+     * The SQLite ALTER TABLE documentation can be found
+     * <a href="http://sqlite.org/lang_altertable.html">here</a>. If you add new columns
+     * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
+     * you can use ALTER TABLE to rename the old table, then create the new table and then
+     * populate the new table with the contents of the old table.
+     * </p><p>
+     * This method executes within a transaction.  If an exception is thrown, all changes
+     * will automatically be rolled back.
+     * </p>
+     *
+     * @param db         The database.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateDatabase(db,oldVersion,newVersion);
+
+    }
+
+    private static void updateData(SQLiteDatabase db,String email,int imageResourceID){
+        db.execSQL("SELECT * USER WHERE EMAIL_ADDRESS = '"+email+"';");
+    }
+    private static void insertData(SQLiteDatabase db, String email,String bitmap){
+        ContentValues contentValue = new ContentValues();
+        contentValue.put("EMAIL_ADDRESS",email);
+        contentValue.put("BITMAP_RESOURCE_ID",bitmap);
+
+        db.insert("USERS",null,contentValue);
+    }
+    private void updateDatabase(SQLiteDatabase db, int oldVersion,int newVersion){
+        if(oldVersion==1){
+            db.execSQL("CREATE TABLE USERS (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "EMAIL_ADDRESS TEXT," +
+                    "BITMAP_RESOURCE_ID TEXT);");
+        }
+    }
 }
--- app/src/main/java/com/automatedcartollingsystem/registration/EditProfileActivity.java
+++ app/src/main/java/com/automatedcartollingsystem/registration/EditProfileActivity.java
@@ -1,14 +1,109 @@
 package com.automatedcartollingsystem.registration;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.automatedcartollingsystem.models.Constants;
+import com.example.automatedcartollingsystem.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+
+    private String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        EditText carBrandEditText = findViewById(R.id.inputCarBrandEditText);
+        EditText carModelEditText = findViewById(R.id.inputCarModelEditText);
+        EditText carRegEditText = findViewById(R.id.inputCarRegistrationEditText);
+
+
+        findViewById(R.id.update_button).setOnClickListener(v -> {
+
+            String carBrandText = carBrandEditText.getText().toString().trim();
+            String carModelText = carModelEditText.getText().toString().trim();
+            String carRegText = carRegEditText.getText().toString().trim();
+
+            if(!carBrandText.equals("")|| !carModelText.equals("")
+            || !carRegText.equals("")){
+                //Main problem is getting the data
+                putCarData(requestQueue,carBrandText,carModelText,carRegText);
+
+            } else {
+                Toast.makeText(this,"Incomplete!",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    //
+    private void putCarData(RequestQueue requestQueue,String carBrand,String carModel,String carReg){
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Constants.URL_CAR_STRING,
+                response -> {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        result = jsonObject.getString("result");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    if(result!=null && result.equals("True")){
+                        Toast.makeText(this,"Successfully updated your details",
+                                Toast.LENGTH_SHORT).show();
+
+                        startActivity(new Intent(EditProfileActivity.this,
+                                ProfileActivity.class));
+                        finish();
+                    } else {
+                        Toast.makeText(this,"Something went wrong, try again",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                }, Throwable::printStackTrace)
+        {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String,String> parameters = new HashMap<>();
+
+                parameters.put("brand",carBrand);
+                parameters.put("model",carModel);
+                parameters.put("registration",carReg);
+                parameters.put("user_id",ProfileActivity.user_id);
+                parameters.put("type","edit");
+
+                return parameters;
+            }
+        };
+        requestQueue.add(stringRequest);
     }
 }
